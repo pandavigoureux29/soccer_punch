4a3db0e5a1f8f1dca69540eb56679b4b0a996129
pandavigoureux29 <pandavigoureux29@gmail.com>
2016-10-11 08:48:13 -0400

    first commit

---
+++ Assets/scripts/utils/SpriteTouchHandler.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpriteTouchHandler : MonoBehaviour {
+
+    [SerializeField] List<GameObject> m_targetsCallback;
+
+    [SerializeField] Collider2D m_collider;
+
+    [SerializeField] string m_onInputDownCallback;
+    [SerializeField] string m_onInputUpCallback;
+
+    bool m_pressed = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (m_collider == null)
+            m_collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check press
+        if (m_pressed == false && IsPressed())
+        {
+            SpriteTouchData touchData = ComputeTouch();
+            //check attack 
+            if (m_collider == touchData.colliderTouched)
+            {
+                m_pressed = true;
+                SendCallbacks(m_onInputDownCallback,touchData);
+            }
+        }
+
+        //Check Release
+        if (m_pressed && IsReleased())
+        {
+            m_pressed = false;
+            SpriteTouchData touchData = ComputeTouch();
+            if (m_collider == touchData.colliderTouched)
+            {
+                SendCallbacks(m_onInputUpCallback,touchData);
+            }
+        }
+    }
+
+    SpriteTouchData ComputeTouch()
+    {
+        SpriteTouchData touchData = new SpriteTouchData();
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(CustomGetTouchPosition());
+        touchData.touchPosition = new Vector2(worldPoint.x, worldPoint.y);
+        touchData.colliderTouched = Physics2D.OverlapPoint(touchData.touchPosition);
+        touchData.sourceComponent = this;
+        return touchData;
+    }
+
+    Vector2 CustomGetTouchPosition()
+    {
+#if UNITY_STANDALONE || UNITY_EDITOR
+        return Input.mousePosition;
+#else
+		return Input.GetTouch(0).position;
+#endif
+    }
+
+    bool IsPressed()
+    {
+#if UNITY_STANDALONE || UNITY_EDITOR
+        return Input.GetMouseButtonDown(0);
+#else
+			return Input.touchCount == 1;
+#endif
+    }
+
+    bool IsReleased()
+    {
+#if UNITY_STANDALONE || UNITY_EDITOR
+        return Input.GetMouseButtonUp(0);
+#else
+	return Input.touchCount < 1 ;
+#endif
+    }
+
+    void SendCallbacks(string callback, SpriteTouchData data)
+    {
+        foreach( var go in m_targetsCallback)
+        {
+            go.SendMessage(callback, data, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    public class SpriteTouchData
+    {
+        public Vector2 touchPosition;
+        public Collider2D colliderTouched;
+        public Component sourceComponent;        
+    }
+}
---
+++ Assets/scripts/utils/UI/UITextNumberScroller.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UITextNumberScroller : MonoBehaviour {
+
+    protected Text m_text;
+
+    protected bool m_scrolling = false;
+
+    protected int m_targetNumber = 0;
+
+    protected float m_timeByUnit = 1;
+    protected float m_time = 0;
+    protected int m_direction = 1;
+    
+    // Use this for initialization
+    void Awake () {
+        m_text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+	    if( m_scrolling )
+        {
+            m_time += Time.deltaTime;
+            if( m_time >= m_timeByUnit)
+            {
+                m_time = 0;
+                int current = int.Parse( m_text.text );
+                current += m_direction;
+                m_text.text = "" + current;
+                if( current == 0)
+                {
+                    _ZeroReached();
+                }
+                if (current == m_targetNumber)
+                {
+                    TargetReached();
+                }
+            }
+        }
+	}
+
+    protected virtual void _ZeroReached()
+    {
+
+    }
+
+    protected virtual void TargetReached()
+    {
+        m_scrolling = false;
+    }
+
+    public virtual void ScrollTo(int _targetNumber, float _duration)
+    {
+        m_targetNumber = _targetNumber;
+        m_scrolling = true;
+
+        var current = int.Parse(m_text.text);
+        //Get direction of the scroll
+        int delta = _targetNumber - current;
+        m_direction = delta < 0 ? -1 : 1;
+        //compute the speed
+        m_timeByUnit = _duration / Mathf.Abs(delta);
+        m_time = 0;
+    }
+    
+    public bool Scrolling
+    {
+        get
+        {
+            return m_scrolling;
+        }        
+    }
+}
