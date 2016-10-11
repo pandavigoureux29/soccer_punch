8309c6586d852b4ca7ba94198641a4083fd40181
pandavigoureux29 <pandavigoureux29@gmail.com>
2016-10-11 11:42:06 -0400

    first draft server communication

---
+++ Assets/scripts/PlayerSpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerSpawnManager : NetworkBehaviour {
+
+    List<Transform> m_teams;
+    
+	// Use this for initialization
+	void Start () {
+        m_teams = new List<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    
+    public bool SpawnPlayer(string _pitchPlayerPath)
+    {
+        CmdSpawnPlayer(_pitchPlayerPath);
+        return true;
+    }
+
+    [Command]
+    void CmdSpawnPlayer(string _playerPrefab)
+    {
+       var player = Instantiate( Resources.Load("prefabs/players/"+_playerPrefab ) ) as GameObject;
+       NetworkServer.Spawn(player);
+    }
+}
---
+++ Assets/scripts/UI/UIDraggablePlayer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class UIDraggablePlayer : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    [SerializeField] string pitchPlayerPath;
+
+    PlayerSpawnManager m_spawnManager;
+    Vector3 m_initialPosition;
+        
+    public void OnBeginDrag(PointerEventData _eventData)
+    {
+        m_initialPosition = transform.localPosition;
+    }
+
+    public void OnDrag(PointerEventData _eventData)
+    {
+        this.transform.position = _eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData _eventData)
+    {
+        if( m_spawnManager == null )
+            m_spawnManager = Component.FindObjectOfType<PlayerSpawnManager>();
+
+        var success = m_spawnManager.SpawnPlayer(pitchPlayerPath);
+        if( success)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.localPosition = m_initialPosition;
+        }
+    }
+}
