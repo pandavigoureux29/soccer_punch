f18d6c6396ed423725a9aa3df88a3c2712657069
Medulli <ju.michelet@gmail.com>
2016-10-11 12:09:41 -0400

    Scene WIP
    + Scene with basic assets
    + Goal component
    + Score component

---
+++ Assets/scripts/GoalComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GoalComponent : MonoBehaviour {
+
+    //private GameObject ball;
+    private ScoreComponent scoreComponent;
+
+    void Start()
+    {
+        scoreComponent = FindObjectOfType<ScoreComponent>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            if (scoreComponent == null)
+                return;
+
+            scoreComponent.ScoreGoal(gameObject.tag);            
+        }
+    }
+}
---
+++ Assets/scripts/ScoreComponent.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ScoreComponent : MonoBehaviour
+{
+    public Text ScoreText;
+
+    private Team team1;
+    private Team team2;
+
+    void Start ()
+    {
+        team1 = new Team("Team1", "Team 1");
+        team2 = new Team("Team2", "Team 2");
+        SetScoreText();
+    }
+    
+    public void ScoreGoal(string teamId)
+    {
+        if(team1.Id == teamId)
+        {
+            team1.Score++;
+        }
+        else
+        {
+            team2.Score++;
+        }
+        SetScoreText();
+    }
+
+    public void SetScoreText()
+    {
+        ScoreText.text = team1.DisplayName + ": " + team1.Score.ToString() + "\n" + team2.DisplayName + ": " + team2.Score.ToString();
+    }
+}
+
+public class Team
+{
+    public string DisplayName;
+    public string Id;
+    public int Score;
+    public List<string> Players;
+
+    public Team(string id, string displayName, int score = 0)
+    {
+        Id = id;
+        DisplayName = displayName;
+        Score = score;
+    }
+}
