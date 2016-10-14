d97232e3061a3019826ee05554431452e2da9ecb
pandavigoureux29 <pandavigoureux29@gmail.com>
2016-10-14 11:05:47 -0400

    new ui for network

---
+++ Assets/scripts/UI/CustomNetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class CustomNetworkManager : MonoBehaviour {
+
+    [SerializeField] InputField m_inputfield;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnStartHostButtonClicked()
+    {
+        NetworkManager.singleton.StartHost();
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnStartClientButtonClicked()
+    {
+        NetworkManager.singleton.networkAddress = m_inputfield.text ;
+        NetworkManager.singleton.StartClient();
+        this.gameObject.SetActive(false);
+    }
+    
+}
